0e141b70a8896ae9d46323beef541a4bf5f7e656
Jason Jun <junjason@umich.edu>
2023-09-12 15:06:16 -0400

    part one phase one complete

---
+++ practice-for-ch-ruby-chess-long-practice-main/board.rb
@@ -0,0 +1,50 @@
+require_relative "piece.rb"
+
+class Board
+    def initialize
+        @rows = Array.new(8) {Array.new(8)}
+
+        (0..1).each do |row|
+            (0..7).each do |col|
+                @rows[row][col] = Piece.new
+            end
+        end
+
+        (-2..-1).each do |row|
+            (0..7).each do |col|
+                @rows[row][col] = Piece.new
+            end
+        end
+    end
+
+    def [](pos)
+        row, col = pos
+        @rows[row][col]
+    end
+
+    def []=(pos, val)
+        row, col = pos
+        @rows[row][col] = val
+    end
+
+    def move_piece(start_pos, end_pos)
+        if self[start_pos] == nil
+            raise RuntimeError.new("There is no piece at start_pos")
+        end
+
+        if !valid_pos?(end_pos)
+            raise RuntimeError.new("There is position at #{end_pos}")
+        end
+
+        self[end_pos] = self[start_pos]
+        self[start_pos] = nil
+    end
+
+    def valid_pos?(pos)
+        row, col = pos
+        return true if row >= 0 && row < 8 && col >= 0 && col < 8
+        
+    end
+
+
+end
---
+++ practice-for-ch-ruby-chess-long-practice-main/piece.rb
@@ -0,0 +1,8 @@
+class Piece
+
+    def inspect 
+        "#{object_id[0..5]}"
+    end
+
+
+end
